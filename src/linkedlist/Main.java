15f484c64899801af91fec481c1ac27b15b1615d
DekyyEx <vlad.moor123@mail.ru>
2024-02-11 19:42:14 +0300

    HomeWorkOOP4

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -3,11 +3,11 @@ package linkedlist;
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(10);
+        list2.add(60);
         list2.add(36);
-        list2.add(56);
-        list2.add(76);
-        list2.add(34);
-        list2.add(88);
+        list2.add(54);
+        list2.add(93);
 
         System.out.println(list2);
         System.out.println("Index[0]=" + list2.get(0) + " Index[1]=" + list2.get(1) + " Index[2]=" + list2.get(2) +
